9863ff856faaa9b837d228650556de40e33cfcb1
arvindk99 <45133989+arvindk99@users.noreply.github.com>
2020-02-22 09:51:34 -0800

    Update circle.h

--- circle.h
+++ circle.h
@@ -1,6 +1,35 @@
-#ifndef CIRCLE_H_INCLUDED
-#define CIRCLE_H_INCLUDED
+//
+//  Circle.h
+//  Assignment5
+//
 
+#ifndef Circle_h
+#define Circle_h
+#include "BasicShape.h"
 
+class Circle: public BasicShape
+{
+private:
+    long centerX;
+    long centerY;
+    double radius;
+    
+public:
+    Circle() { centerX = centerY = 0; radius = 0;}
+    Circle(long x, long y, double r)
+    {
+        centerX = x;
+        centerY = y;
+        radius = r;
+    }
+    
+    void setCenterX(long x) { centerX = x; }
+    long getCenterX(){ return centerX; }
+    
+    void setCenterY(long y) { centerY = y;}
+    long getCenterY(){ return centerY; }
+    
+    double calcArea() { return 3.14159 * radius * radius; }
+};
 
-#endif // CIRCLE_H_INCLUDED
+#endif /* Circle_h */
