1c13bdb53ed314c4cd27ab039b01f11d82ab275f
amir tadros <amirtadros01@gmail.com>
2020-02-21 16:03:25 -0800

    Setup Rectangle Class
    Setup the rectangle class with a private long int width and long int length, public get functions for width and length, a constructor that sets the width and length, and a calcArea function that returns the area of the rectangle.

--- rectangle.h
+++ rectangle.h
@@ -1,6 +1,18 @@
 #ifndef RECTANGLE_H_INCLUDED
 #define RECTANGLE_H_INCLUDED
 
-
+class rectangle {
+private:
+    long int width;
+    long int length;
+public:
+    rectangle(long int w, long int l){
+        width = w;
+        length = l;
+    }
+    long int getWidth(){return (width);}
+    long int getLength(){return (length);}
+    long int calcArea(){return (width * length);}
+};
 
 #endif // RECTANGLE_H_INCLUDED
