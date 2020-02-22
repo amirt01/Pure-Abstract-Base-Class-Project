016fff8c730857b42e10d8ddfafa89cadc96eaf8
arvindk99 <45133989+arvindk99@users.noreply.github.com>
2020-02-22 09:50:51 -0800

    Update basicShape.h

--- basicShape.h
+++ basicShape.h
@@ -1,6 +1,24 @@
-#ifndef BASICSHAPE_H_INCLUDED
-#define BASICSHAPE_H_INCLUDED
+//
+//  BasicShape.h
+//  Assignment5
+//
 
+#ifndef BasicShape_h
+#define BasicShape_h
 
+#include <iostream>
+#include <string>
+using namespace std;
 
-#endif // BASICSHAPE_H_INCLUDED
+class BasicShape
+{
+private:
+    double area;
+public:
+    //Default constructor
+    BasicShape(){ area = 0; }
+    
+    double getArea() { return area; }
+    virtual double calcArea() = 0;
+};
+#endif /* BasicShape_h */
