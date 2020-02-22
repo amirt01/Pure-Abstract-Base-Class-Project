7dc2dbc2dc5ed91b42771be13ee9fae8ad4a61d2
Timothy Hyatt <timhyatt707@gmail.com>
2020-02-22 14:30:07 -0800

    Header documentation and fix for circle class

--- basicShape.h
+++ basicShape.h
@@ -1,7 +1,16 @@
-//
-//  BasicShape.h
-//  Assignment5
-//
+/*
+
+  CIS 22B
+
+    Lab5 - Basic Shape: The Basic Shape class that Circle
+    and Rectangle class inherits from. It stores the area
+    of the shape and calculates it. 
+
+  Author: Amir Tadros, Anh Hoang Mai Le, Arvind Kumar, Timothy Hyatt
+
+  Date: February 22, 2020
+
+*/
 
 #ifndef BasicShape_h
 #define BasicShape_h
@@ -15,10 +24,13 @@ class BasicShape
 protected:
     double area;
 public:
-    //Default constructor
+    // Default constructor
     BasicShape(){ area = 0; }
 
+    // Returns the area of the shape
     double getArea() { return area; }
+
+    // Calculates the area of the shape
     virtual void calcArea() = 0;
 };
 #endif /* BasicShape_h */
