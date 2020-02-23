bbf8d4998f732d3502b97f1da66e73245b3cc716
arvindk99 <45133989+arvindk99@users.noreply.github.com>
2020-02-22 20:40:05 -0800

    Update circle.h

--- circle.h
+++ circle.h
@@ -1,20 +1,16 @@
 /*
-
   CIS 22B
-
     Lab5 - Circle: This class inherits from the Basic Shape
     class and constructs a class which calculates the area of
     a circle.
-
   Author: Amir Tadros, Anh Hoang Mai Le, Arvind Kumar, Timothy Hyatt
-
   Date: February 22, 2020
-
 */
 
 #ifndef Circle_h
 #define Circle_h
 #include <cmath>
+#include "BasicShape.h"
 
 class Circle: public BasicShape
 {
@@ -36,18 +32,17 @@ public:
         calcArea();
     }
 
-    // Set center x axis
-    void setCenterX(long x) { centerX = x; }
     // Get center x axis
     long getCenterX(){ return centerX; }
 
-    // Set center y axis
-    void setCenterY(long y) { centerY = y;}
     // Get center x axis
     long getCenterY(){ return centerY; }
 
     // Calculate the area using M_PI
-    void calcArea() { area = M_PI * radius * radius; }
+    void calcArea()
+    {
+        area = M_PI * radius * radius;
+    }
 };
 
 #endif /* Circle_h */
