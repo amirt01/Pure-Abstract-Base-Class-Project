8d524cc85689106dc24e55a37d6df3a9e33418e9
Timothy Hyatt <timhyatt707@gmail.com>
2020-02-24 10:23:14 -0800

    Final version of circle.h

--- circle.h
+++ circle.h
@@ -3,7 +3,7 @@
     Lab5 - Circle: This class inherits from the Basic Shape
     class and constructs a class which calculates the area of
     a circle.
-  Author: Amir Tadros, Anh Hoang Mai Le, Arvind Kumar, Timothy Hyatt
+  Author: Timothy Hyatt - Others: Amir Tadros, Anh Hoang Mai Le, Arvind Kumar
   Date: February 22, 2020
 */
 
@@ -31,13 +31,33 @@ public:
         calcArea();
     }
 
-    // Get center x axis
+    /*
+
+      Gets the the center x coordinate
+
+      Return value: long - The center x coord
+
+    */
     long getCenterX(){ return centerX; }
 
-    // Get center x axis
+    /*
+
+      Gets the the center y coordinate
+
+      Return value: long - The center y coord
+
+    */
     long getCenterY(){ return centerY; }
 
-    // Calculate the area using M_PI
+    /*
+
+      Calculate the area of the circle by multiplying
+
+      the radius to the power of 2 and PI.
+
+      Return value: void
+
+    */
     void calcArea()
     {
         area = M_PI * radius * radius;
