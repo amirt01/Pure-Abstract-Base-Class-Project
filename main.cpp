3b438fe42cc0f8602d489e90363609d7abb7ef1a
anhle126 <61336972+anhle126@users.noreply.github.com>
2020-02-23 18:12:16 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include "circle.h"
-#include "rectangle.h"
-
 #include <iostream>
+#include "BasicShape.h"
+#include "Circle.h"
+#include "Rectangle.h"
 
 using namespace std;
 
-int main (){
-
+int main()
+{
+    Circle mycircle(1,2,3);
+    Rectangle myrectangle(2, 7);
+    mycircle.calcArea();
+    myrectangle.calcArea();
+    cout<<"A circle with center ("<<mycircle.getCenterX()<<","<<mycircle.getCenterY()
+    <<") and radius = 3 has an area = "<<mycircle.getArea()<<endl;
+    cout<<"A rectangle with size "<<myrectangle.getWidth()<<"x"<<myrectangle.getLength()
+    <<" has an area = "<<myrectangle.getArea()<<endl;
+    return 0;
 }
